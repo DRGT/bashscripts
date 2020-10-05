ac0c9166f37642cabd591293181d0d1173bdd225
DRGT <28861839+DRGT@users.noreply.github.com>
2020-10-05 12:57:22 +0200

    Update mount_azblob.sh

--- mount_azblob.sh
+++ mount_azblob.sh
@@ -1,7 +1,5 @@
-GREEN='\033[0;32m'
-NC='\033[0m'
 useradd -m sftpuser
-echo "${GREEN}Type in password for sftpuser${NC}"
+echo "Type in password for sftpuser"
 passwd sftpuser
 service sshd restart
 wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
@@ -10,11 +8,11 @@ apt-get update
 apt install blobfuse
 mkdir /mnt/resource/blobfusetmp -p
 chown sftpuser /mnt/resource/blobfusetmp
-echo "${GREEN}Type in Storage Account Name${NC}"
+echo "Type in Storage Account Name"
 read accountName
-echo "${GREEN} Type in Storage Account Key${NC}"
+echo "Type in Storage Account Key"
 read accountKey
-echo "${GREEN} Type in Storage Container Name${NC}"
+echo "Type in Storage Container Name"
 read containerName
 echo accountName $accountName > /home/sftpuser/fuse_connection.cfg
 echo accountKey $accountKey >> /home/sftpuser/fuse_connection.cfg
